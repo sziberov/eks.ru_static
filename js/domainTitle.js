58216ba68990fa73d28530c2850d65d8f8f43bb4
KAYOver <sziberov@gmail.com>
2017-03-04 23:51:02 +0200

    Fixed domainTitle.js

--- js/domainTitle.js
+++ js/domainTitle.js
@@ -8,13 +8,8 @@ if (document.title == '') {
 	document.title += "EKS.RU";
 }
 else {
-	if(window.location.href.indexOf("software") > -1) {
-		if(location.pathname.substr(location.pathname.lastIndexOf("/")+1) == 'index.html') {
-			document.title += " @ EKS.RU";
-		}
-		else {
+	if(window.location.href.indexOf("/software/") > -1) {
 		document.title += " - Программы @ EKS.RU";
-		}
 	}
 	else {
 		document.title += " @ EKS.RU";
