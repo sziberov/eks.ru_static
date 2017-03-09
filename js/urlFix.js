5d46064a6a4c7f28859bfe25b200f685495c0d94
KAYOver <sziberov@gmail.com>
2017-03-09 19:02:03 +0200

    ...

--- js/urlFix.js
+++ js/urlFix.js
@@ -5,7 +5,7 @@
 */
 
 function urlFix() {
-	var link = window.location.href;
+	var link = window.location.host + window.location.pathname;
 	link.slice(0, -5);
 	window.history.replaceState( null, null, link );
 };
