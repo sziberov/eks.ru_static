7666f5e02e0ba2a1e100d22494ca0a3933721e37
KAYOver <sziberov@gmail.com>
2017-03-13 20:31:34 +0200

    Added Discord | ...

--- js/header.js
+++ js/header.js
@@ -4,6 +4,25 @@
 ** (c) kayover
 */
 
+/*
+(function() {
+	var scriptEls = document.getElementsByTagName( 'script' );
+	var thisScriptEl = scriptEls[scriptEls.length - 1];
+	var scriptPath = thisScriptEl.src;
+	var scriptFolder = scriptPath.substr(0, scriptPath.lastIndexOf( '/' )+1 );
+	var root = scriptFolder.substring(0, scriptFolder.lastIndexOf('js/'));
+
+	$.ajaxPrefilter(function( options, original_Options, jqXHR ) {
+		options.async = true;
+	});
+	document.write('<div id="header">');
+
+	$.get(root + "header.html", function(data){
+		$("#header").replaceWith(data);
+	});
+})();
+*/
+
 $.ajaxPrefilter(function( options, original_Options, jqXHR ) {
 	options.async = true;
 });
